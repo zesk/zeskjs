33c045133bcf7bc72ca7f19fe0eca94b8f505928
Kent Davidson <kent@marketacumen.com>
2017-09-13 18:07:58 -0400

    allow inheritance without class

---
+++ src/Widgets.js
@@ -0,0 +1,6 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) 2017 Market Acumen, Inc. All rights reserved
+ */
+module.exports = {};
---
+++ src/index.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright &copy; 2017 Market Acumen, Inc.
+ */
+module.exports = {
+	HTML: require('./HTML'),
+	Locale: Locale,
+	URL: URL,
+	Widgets: require('./Widgets'),
+	Zesk: require('./Zesk'),
+	ZeskException: require('./ZeskException'),
+	ZeskHandleJSON: require('./ZeskHandleJSON'),
+	ZeskObject: require('./ZeskObject'),
+	MemberTypes: require('./MemberTypes'),
+};
+
